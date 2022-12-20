040c31aaf1377fd0cbd064bf82932e742d6819f6
erikdelbom <erikdelbom@gmail.com>
2022-12-20 20:40:28 +0100

    test input works

--- 2022/16/sol.py
+++ 2022/16/sol.py
@@ -71,70 +71,90 @@ def all_open(opened):
 
 def get_minute_score(valves, opened):
     sum = 0
-    for key in valves.keys():
-        if opened[key]:
+    for key in opened.keys():
+        if valves[key].flow_rate >= 1 and opened[key]:
             sum += valves[key].flow_rate 
     return sum
 
 def predicted_value():
     pass
 
+def calc_score_lims():
+    pass
+
 def search(valves, start, minute, opened, path_key, paths, score):
-    score += get_minute_score(valves, opened)
-    path_key += start
-    previous = path_key[-4:-2]
+    score[minute] = get_minute_score(valves, opened)
+
+    if sum(score) < 20 and minute > 4:
+        return
+    if sum(score) < 300 and minute > 15:
+        return
 
     if all_open(opened):
-        paths[path_key] = score + (30-minute+1) * get_minute_score(valves, opened)    
+        for i in range(30-minute):
+            score[minute+i] = get_minute_score(valves, opened)
+        paths[path_key] = score
+        return
     elif minute >= 29:
+        #score[minute] = get_minute_score(valves, opened)
         paths[path_key] = score 
         return 
-    
-    other_found = False
-    for n in valves[start].neighbors:
-        if n == previous:
-            continue
-        other_found = True
-        search(valves, n, minute+1, copy(opened), path_key, paths, score)
-        if valves[n].flow_rate > 0 and not opened[n]:
-            score += get_minute_score(valves, opened)
-            opened_copy = copy(opened)
-            opened_copy[n] = True
-            if minute < 28:
-                search(valves, n, minute+2, opened_copy, path_key+'!', paths, score)
-    
-    if not other_found:
-        search(valves, previous, minute+1, copy(opened), path_key, paths, score)
-        if valves[previous].flow_rate > 0 and not opened[previous]:
-            score += get_minute_score(valves, opened)
-            opened_copy = copy(opened)
-            opened_copy[previous] = True
-            #score -= valves[previous].flow_rate
-            if minute < 28:
-                search(valves, previous, minute+2, opened_copy, path_key+'!', paths, score)
-        
+
+    #print(path_key)
+
+    for node in opened.keys():
+        if node != start:
+            if len(path_key) >= 3*2:
+                path_key_copy = path_key.replace('!', '')
+                p1 = path_key_copy[-6:-4]
+                p2 = path_key_copy[-4:-2]
+                p3 = path_key_copy[-2:]
+                if p1 == p3 and node == p2:
+                    continue
+            distance = valves[start].distances[node]
+            if valves[node].flow_rate >= 15:
+                if not opened[node] and 29-minute > distance+1:
+                    for i in range(distance+1):
+                        score[minute+i] = get_minute_score(valves, opened)
+                    opened_copy = copy(opened)
+                    opened_copy[node] = True
+                    search(valves, node, minute+distance+1, opened_copy, path_key+'!'+node, paths, copy(score))
+            else:
+                if 29 - minute > distance:
+                    for i in range(distance+1):
+                        score[minute+i] = get_minute_score(valves, opened)
+                    search(valves, node, minute+distance, copy(opened), path_key+node, paths, copy(score))
+                if not opened[node] and 29-minute > distance+1:
+                    opened_copy = copy(opened)
+                    opened_copy[node] = True
+                    search(valves, node, minute+distance+1, opened_copy, path_key+'!'+node, paths, copy(score))
 
 def part_1(valves):
     # Calculate distance between all valves
-    # for key in valves.keys():
-    #    for goal in valves.keys():
-    #        valves[key].distances[goal] = len(bfs(valves, key, goal))
-    #    valves[key].print()    
+    for key in valves.keys():
+        for goal in valves.keys():
+            valves[key].distances[goal] = len(bfs(valves, key, goal))
         
     paths = {}
     opened = {}
     for key in valves.keys():
-        opened[key] = False
+        for n in valves[key].neighbors:
+            if valves[n].flow_rate < 1:
+                #print('remove', n)
+                valves[key].neighbors.remove(n)
+        if valves[key].flow_rate > 0:
+            opened[key] = False
 
-    search(valves, 'AA', 0, opened, '', paths, 0)
-    
+    #print(opened)
+
+    search(valves, 'AA', 0, opened, '', paths, [0 for i in range(30)])
+    print('search done')
     max_score = 0
     max_key = ''
     for key in paths.keys():
-        if paths[key] > max_score:
-            max_score = paths[key]
+        if sum(paths[key]) > max_score:
+            max_score = sum(paths[key])
             max_key = key
-    print(max_key)
     return max_score
 
 print('Part 1:', part_1(read_input()))
