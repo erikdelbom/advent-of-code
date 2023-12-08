c81e76e4e6e959dfed28ed6af172a0801de72f50
Erik Delbom <erik@macbook.home>
2023-12-08 15:30:57 +0100

    started day7

---
+++ 2023/07/solution.py
@@ -0,0 +1,26 @@
+import sys
+import time
+
+def read_input(filename):
+    with open(filename, 'r') as f:
+        return list(map(str.rstrip, f.readlines()))
+
+
+value = { '2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7, 'T': 8, 'J': 9, 'Q': 10, 'K': 11, 'A': 12 }
+
+
+def part_1(data):
+    
+
+def part_2(data):
+    return None
+
+input_file = sys.argv[1]
+
+start_time = time.time()
+print("Part 1:", part_1(read_input(input_file)), end=" - ") 
+print(round((time.time() - start_time), 3), "s")
+
+start_time = time.time()
+print("Part 2:", part_2(read_input(input_file)), end=" - ")
+print(round((time.time() - start_time), 3), "s")
