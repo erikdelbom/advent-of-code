f0041cd6e175b5d9bee98fa00973217b16cc17db
Erik Delbom <erikdelbom@gmail.com>
2024-12-03 09:36:26 +0100

    day 2 done

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -6,53 +6,41 @@ def read_input(filename):
     with open(filename, 'r') as f:
         return list(map(str.rstrip, f.readlines()))
 
-def valid(diff, deriv, i):
-    if not (abs(diff) > 0 and abs(diff) < 4):
-        return False
+def valid(report):
+    valid = 1
+    prev_diff = report[0] - report[1]
+    for i in range(len(report)-1):
+        diff = report[i] - report[i+1]
+        if not (prev_diff * diff > 0 and abs(diff) < 4):
+            valid = 0
+            break
+    return valid
 
-    if deriv
-
+def sub_reports(report):
+    return [report[:i] + report[i+1:] 
+           for i in range(len(report))]
 
 def part_1(data):
     count = 0
     for report in data: 
-        report = np.array(list(map(int, report.split())))
-        roll = np.roll(report, -1)[:-1]
-        diffs = report[:-1] - roll
-        
-        neg = np.sum(diffs < 0)
-        pos = np.sum(diffs > 0)
-
-        if not (neg == len(diffs) or pos == len(diffs)):
-            continue
-
-        if np.all(np.abs(diffs) <= 3):
-            count += 1
-
+        report = list(map(int, report.split()))
+        count += valid(report)
     return count 
 
 def part_2(data):
     count = 0
     for report in data: 
         report = list(map(int, report.split()))
-        deriv = 0
-        for i in range(len(report)-1):
-            diff = report[i] - report[i-1]
-            
-            d = 0
-            if diff != 0:
-                d = 1 if abs(diff) == diff else -1
-            deriv += d
-
-            if not (deriv == i or deriv == -i):
-                break
-
-            if abs(diff)
-            
-
-
-
+        if valid(report) == 0:
+            subs = sub_reports(report)
+            for sub in subs:
+                if valid(sub) == 1:
+                    count += 1
+                    break
+        else:
+            count += 1
     return count 
+
 
 input_file = sys.argv[1]
 
