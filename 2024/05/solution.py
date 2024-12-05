831245c256f32929f27bf4b9b2e169afc63add43
Erik Delbom <erikdelbom@gmail.com>
2024-12-05 21:28:42 +0100

    day5 done

--- 2024/05/solution.py
+++ 2024/05/solution.py
@@ -19,11 +19,20 @@ def split_list(lst, delim):
         result.append(current)
     return result
 
-def sort_rules(rules):
-    sorted = []
-    for a in rules.keys():
-        for r in rules[a]:
-            
+def sort(page, rules):
+    sorted = [0 for _ in range(len(page))]
+    counts = {}
+    for p in page:
+        count = 0 
+        for c in page:
+            if c in rules[p]:
+                count += 1
+        counts[p] = count
+    
+    for p in page:
+        sorted[counts[p]] = p
+
+    return sorted
 
 def part_1(data):
     rules_in, pages = split_list(data, '')
@@ -34,12 +43,43 @@ def part_1(data):
             rules[a].append(b)
         except:
             rules[a] = [b]
+   
+    sum = 0
+    for page in pages:
+        valid = True
+        page = list(map(int, page.split(',')))
+        for i in range(len(page)-1):
+            for j in range(i+1, len(page)):
+                if page[j] not in rules[page[i]]:
+                    valid = False
+        if valid:
+            sum += page[len(page)//2]
 
-    for key in rules.keys():
-        print(key, rules[key])
+    return sum
    
 def part_2(data):
-    return None
+    rules_in, pages = split_list(data, '')
+    rules = {}
+    for r in rules_in:
+        a, b = map(int,r.split('|'))
+        try:
+            rules[a].append(b)
+        except:
+            rules[a] = [b]
+   
+    sum = 0
+    for page in pages:
+        valid = True
+        page = list(map(int, page.split(',')))
+        for i in range(len(page)-1):
+            for j in range(i+1, len(page)):
+                if page[j] not in rules[page[i]]:
+                    valid = False
+        if not valid:
+            correct = sort(page, rules)
+            sum += correct[len(page)//2]
+                    
+    return sum
 
 input_file = sys.argv[1]
 
