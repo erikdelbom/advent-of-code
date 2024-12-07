7622a00fbc7dcb931d2d3a8c2a5aace2794e2fb6
Erik Delbom <erikdelbom@gmail.com>
2024-12-07 01:13:35 +0100

    day 6 done

--- 2024/06/solution.py
+++ 2024/06/solution.py
@@ -13,45 +13,21 @@ def find_start(data):
                 return y, x
     return -1, -1
 
-def get_dir(c):
-    if c == '<':
-        return 0, -1
-    elif c == '>':
-        return 0, 1
-    elif c == '^':
-        return -1, 0
-    elif c == 'v':
-        return 1, 0
-    else:
-        print("Invalid direction character:", c)
-        exit()
-
-def rotate(c):
-    if c == '<':
-        return '^'
-    elif c == '>':
-        return 'v'
-    elif c == '^':
-        return '>'
-    elif c == 'v':
-        return '<'
-    else:
-        print("Invalid direction character:", c)
-        exit()
-     
+get_offset = {'<': (0, -1), '>': (0, 1), '^':(-1, 0), 'v': (1, 0)}
+rotate = {'<': '^', '>': 'v', '^': '>', 'v': '<'}
 
 def part_1(data):
     y, x = find_start(data)
     dir = data[y][x]
     visited = [[0 for _ in range(len(data[0]))] for _ in range(len(data))]
     while True:
-        dy, dx = get_dir(dir) 
+        dy, dx = get_offset[dir] 
         visited[y][x] = 1
         if y+dy >= len(data) or y+dy < 0 or x+dx >= len(data[0]) or x+dx < 0:
             break
         if data[y+dy][x+dx] == '#':
-            dir = rotate(dir)
-            dy, dx = get_dir(dir)
+            dir = rotate[dir]
+            dy, dx = get_offset[dir]
         
         y = y+dy
         x = x+dx
@@ -59,24 +35,71 @@ def part_1(data):
     visited = np.array(visited)
     return np.sum(visited)
 
-def part_2(data):
+def get_visited(data):
     y, x = find_start(data)
     dir = data[y][x]
     visited = [[0 for _ in range(len(data[0]))] for _ in range(len(data))]
     while True:
-        dy, dx = get_dir(dir) 
+        dy, dx = get_offset[dir] 
         visited[y][x] = 1
         if y+dy >= len(data) or y+dy < 0 or x+dx >= len(data[0]) or x+dx < 0:
             break
         if data[y+dy][x+dx] == '#':
-            dir = rotate(dir)
-            dy, dx = get_dir(dir)
+            dir = rotate[dir]
+            dy, dx = get_offset[dir]
         
         y = y+dy
         x = x+dx
     
-    visited = np.array(visited)
-    return np.sum(visited)
+    return visited
+
+
+
+def printmap(data):
+    for line in data:
+        for c in line:
+            print(c, end='')
+        print()
+
+import copy
+# 1911
+def part_2(data):
+    vis = get_visited(data) 
+    count = 0
+    p_count = 0
+    data = [list(s) for s in data]
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            y, x = find_start(data)
+            if (i == y and j == x) or (vis[i][j] == 0):
+                continue
+            data[i][j] = '#'
+            
+            dir = data[y][x]
+            start = time.time()
+            elapsedTime = 0
+            while True:
+                if elapsedTime > 0.02:
+                    count += 1
+                    data[i][j] = '.'
+                    break
+
+                dy, dx = get_offset[dir] 
+                
+                if y+dy >= len(data) or y+dy < 0 or x+dx >= len(data[0]) or x+dx < 0:
+                    data[i][j] = '.'
+                    break
+                
+                while data[y+dy][x+dx] == '#':
+                    dir = rotate[dir]
+                    dy, dx = get_offset[dir]
+                 
+                y = y+dy
+                x = x+dx
+                
+                elapsedTime = time.time() - start
+            p_count += 1 
+    return count
 
 
 input_file = sys.argv[1]
@@ -87,4 +110,4 @@ print(round((time.time() - start_time), 3), "s")
 
 start_time = time.time()
 print("Part 2:", part_2(read_input(input_file)), end=" - ")
-print(round((time.time() - start_time), 3), "s")
+print(round((time.time() - start_time), 6), "s")
