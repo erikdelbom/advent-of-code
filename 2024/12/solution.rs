bd0279e0aec80469dbee8e25acdbbe3ee67ad225
Erik Delbom <erikdelbom@gmail.com>
2024-12-15 15:34:16 +0100

    working on day12

--- 2024/12/solution.rs
+++ 2024/12/solution.rs
@@ -60,26 +60,27 @@ fn find_neighbors(map: &Vec<Vec<char>>, pos: Coord) -> Vec<Coord> {
     neighbors 
 }
 
-fn find_area(map: &Vec<Vec<char>>, visited: &mut Vec<Vec<i32>>, start: Coord) -> (i64, i64) {
+fn find_area(map: &Vec<Vec<char>>, visited: &mut Vec<Vec<i32>>, start: Coord) -> (i64, i64, Vec<Coord>) {
     let (y, x) = start;  
     if visited[y][x] != 0 {
-        return (0, 0);
+        return (0, 0, Vec::new());
     } 
     let area_code = map[y][x];
     let mut frontier: VecDeque<Coord> = VecDeque::new(); 
     frontier.push_back(start);
     visited[y][x] = 1;
     
+    let mut border = Vec::new();
     let mut area = 0;
     let mut per = 0;
     while !frontier.is_empty() {
         let current = frontier.pop_front().unwrap();
         area += 1;
         let neighbors = find_neighbors(map, current);
-        per += 4 - neighbors.len() as i64;
+        let mut per_update = 4 - neighbors.len() as i64;
         for n in neighbors {
             if map[n.0][n.1] != area_code {
-                per += 1;
+                per_update += 1;
                 continue
             }
             if visited[n.0][n.1] == 0 {
@@ -87,45 +88,44 @@ fn find_area(map: &Vec<Vec<char>>, visited: &mut Vec<Vec<i32>>, start: Coord) ->
                 frontier.push_back(n);
             }
         }
+        if per_update != 0 {
+            border.push(current);
+        }
+        per += per_update;
     }
 
-    (area, per)
+    (area, per, border)
 }
 
-
 fn find_sides(map: &Vec<Vec<char>>, start: Coord) -> i64 {
-    let NORTH = 0;
-    let EAST = 1;
-    let SOUTH = 2;
-    let WEST = 3;
-    let offset: Vec<(i32, i32)> = vec![(-1, 0), (1, 0), (0, 1), (0, -1)];
-    let mut dir = EAST;
-    let (mut y, mut x) = (start.0 as i32, start.1 as i32);
-    let mut sides = 1;
-    let area_code = map[y as usize][x as usize];
-    let mut prev_dir = dir;
+    let offsets: Vec<(i32, i32)> = vec![(0,  1), ( 1,  1), ( 1, 0), ( 1, -1), 
+                                        (0, -1), (-1, -1), (-1, 0), (-1,  1)];
+    
+    let mut sides = 0;
+    let mut current = start;
+    let area_code = map[start.0][start.1];
+    let mut dir = 0;
 
-    while dir != NORTH && (y as usize, x as usize) != start {
-        let mut next_found = false; 
-        while !next_found {
-            let (off_y, off_x) = offset[dir as usize];
-            if y+off_y >= 0 && y+off_y < map.len() as i32 &&
-               x+off_x >= 0 && x+off_x < map[0].len() as i32 {
-                let mut neighbor = map[(y+off_y) as usize][(x+off_x) as usize];   
-                if neighbor == area_code {
-                    println!("found neighbor");
-                    (y, x) = (y+off_y, x+off_x);
-                    next_found = true;
-                    if dir != prev_dir {
+    while true {
+        println!("{:?}", current);
+        for i in 0..8 {
+            let n = (current.0 as i32 + offsets[i].0, current.1 as i32 + offsets[i].1);            
+            if n.0 < map.len() as i32 && n.0 >= 0 && n.1 < map[0].len() as i32 && n.1 >= 0 {
+                if map[n.0 as usize][n.1 as usize] == area_code {
+                    current = (n.0 as usize, n.1 as usize);
+                    if i != dir {
                         sides += 1;
                     }
-                    continue;
+                    break;
                 }
-            } 
-            dir = (dir + 1) % 4;
+            }
+        }
+        if current == start && sides > 1 {
+            sides += 1;
+            break;
         }
     }
-    sides + 1
+    sides
 }
 
 fn part_1(data: &Vec<String>) -> i64 {
@@ -134,7 +134,7 @@ fn part_1(data: &Vec<String>) -> i64 {
     let mut visited = vec![vec![0; map[0].len()]; map.len()];
     for i in 0..map.len() {
         for j in 0..map[0].len() {
-            let (area, per) = find_area(&map, &mut visited, (i, j));
+            let (area, per, _) = find_area(&map, &mut visited, (i, j));
             sum += area * per;
         }
     }
@@ -147,11 +147,10 @@ fn part_2(data: &Vec<String>) -> i64 {
     let mut visited = vec![vec![0; map[0].len()]; map.len()];
     for i in 0..map.len() {
         for j in 0..map[0].len() {
-            let (area, _) = find_area(&map, &mut visited, (i, j));
+            let (area, _, border) = find_area(&map, &mut visited, (i, j));
             if area > 0 {
                 let sides = find_sides(&map, (i, j));
                 sum += area * sides;
-                println!("{sides}");
             }
         }
     }
