cf448f21d4997d54b840a66311abd94137bf45df
Erik Delbom <erikdelbom@gmail.com>
2024-12-17 11:06:55 +0100

    day 13 done

--- 2024/13/solution.py
+++ 2024/13/solution.py
@@ -7,21 +7,43 @@ def read_input(filename):
     with open(filename, 'r') as f:
         return list(map(str.rstrip, f.readlines()))
 
-def gcdExtended(a, b): 
-    # Base Case 
-    if a == 0 : 
-        return b,0,1
-             
-    gcd,x1,y1 = gcdExtended(b%a, a) 
-     
-    # Update x and y using results of recursive 
-    # call 
-    x = y1 - (b//a) * x1 
-    y = x1 
-     
-    return gcd,x,y 
+def extended_gcd(a, b):
+    if b == 0:
+        return a, 1, 0
+    gcd, x1, y1 = extended_gcd(b, a % b)
+    x = y1
+    y = x1 - (a // b) * y1
+    return gcd, x, y
+
+def solve_diophantine(a, b, c):
+    gcd, x0, y0 = extended_gcd(a, b)
+    if c % gcd != 0:
+        return (None, None, None)
+    scale = c // gcd
+    return x0 * scale, y0 * scale, gcd
+
+
+def solve_system(ax, bx, cx, ay, by, cy):
+    x1, y1, gcd1 = solve_diophantine(ax, bx, cx)
+    if x1 is None:
+        return 0, 0
+    ax1 = bx // gcd1
+    bx1 = -ax // gcd1
+
+    a2_new = ay * ax1 + by * bx1
+    c2_new = cy - ay * x1 - by * y1
+
+    t0 = c2_new // a2_new
+    if c2_new % a2_new != 0:
+        return 0, 0
+
+    x = x1 + t0 * ax1
+    y = y1 + t0 * bx1
+
+    return x, y
 
 def part_1(data):
+    res = 0
     for i in range(0, len(data), 4):
         ax = int(re.search(r"X\+[0-9]+", data[i]).group().split('+')[1])
         ay = int(re.search(r"Y\+[0-9]+", data[i]).group().split('+')[1])
@@ -30,15 +52,23 @@ def part_1(data):
         cx = int(re.search(r"X\=[0-9]+", data[i+2]).group().split('=')[1])
         cy = int(re.search(r"Y\=[0-9]+", data[i+2]).group().split('=')[1])
         
-        gcdx, a, b = gcdExtended(ax, bx)
-        if cx % gcdx != 0:
-            continue
-        print(gcdx, a, b, cx)
-        print(a * (cx // gcdx), b * (cx // gcdx)) 
-
+        x, y = solve_system(ax, bx, cx, ay, by, cy)
+        res += 3*x + y
+    return res
 
 def part_2(data):
-    return None
+    res = 0
+    for i in range(0, len(data), 4):
+        ax = int(re.search(r"X\+[0-9]+", data[i]).group().split('+')[1])
+        ay = int(re.search(r"Y\+[0-9]+", data[i]).group().split('+')[1])
+        bx = int(re.search(r"X\+[0-9]+", data[i+1]).group().split('+')[1])
+        by = int(re.search(r"Y\+[0-9]+", data[i+1]).group().split('+')[1])
+        cx = int(re.search(r"X\=[0-9]+", data[i+2]).group().split('=')[1])
+        cy = int(re.search(r"Y\=[0-9]+", data[i+2]).group().split('=')[1])
+        
+        x, y = solve_system(ax, bx, cx+10000000000000, ay, by, cy+10000000000000)
+        res += 3*x + y
+    return res
 
 input_file = sys.argv[1]
 
